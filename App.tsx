eb58c83bc017be8187a5d96c2a1228614c75bf75
Serover <35572082+Serover@users.noreply.github.com>
2022-09-26 11:32:50 +0200

    Early draft of map structure etc

--- App.tsx
+++ App.tsx
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+
+import MapScreen from "./screens/MapScreen";
+import LoginScreen from "./screens/LoginScreen";
+import QuestionScreen from "./screens/QuestionScreen";
+import ResultsScreen from "./screens/ResultsScreen";
+import OverviewScreen from "./screens/OverviewScreen";
+
+export type RootStackParamList = {
+  Login: undefined;
+  Map: { quizWalkId: number; path?: string };
+  Question: { quizWalkId: number; questionId: number; path?: string };
+  Results: { quizWalkId: number; path?: string };
+  Overview: { quizWalkId: number; path?: string };
+};
+
+const NativeStack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style="light" />
+      <NativeStack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "grey",
+          },
+          headerTintColor: "white",
+          animation: "slide_from_right",
+          animationDuration: 1500,
+          // headerRight: () => <Text>Test</Text>,
+        }}
+      >
+        <NativeStack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: "Login" }}
+        />
+        <NativeStack.Screen
+          name="Map"
+          component={MapScreen}
+          options={({ route }) => ({
+            title: "Map " + route.params.quizWalkId,
+          })}
+        />
+        <NativeStack.Screen
+          name="Question"
+          component={QuestionScreen}
+          options={({ route }) => ({
+            title:
+              "Question " +
+              route.params.questionId +
+              " - " +
+              route.params.quizWalkId,
+          })}
+        />
+        <NativeStack.Screen
+          name="Results"
+          component={ResultsScreen}
+          options={({ route }) => ({
+            title: "Results " + route.params.quizWalkId,
+          })}
+        />
+        <NativeStack.Screen
+          name="Overview"
+          component={OverviewScreen}
+          options={({ route }) => ({
+            title: "Overview " + route.params.quizWalkId,
+          })}
+        />
+      </NativeStack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
