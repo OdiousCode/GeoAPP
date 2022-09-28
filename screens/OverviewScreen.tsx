101d475b3b4b9a9e8a2fab64c5c3e1948958d65b
OdiousCode <gorgande@gmail.com>
2022-09-28 14:13:36 +0200

    First design

--- screens/OverviewScreen.tsx
+++ screens/OverviewScreen.tsx
@@ -1,24 +1,49 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
 import { RootTabsParamList } from '../App';
-
+import { SafeAreaView } from 'react-native-safe-area-context';
 type Props = NativeStackScreenProps<RootTabsParamList, 'Overview'>;
 
 export default function OverviewScreen({ navigation, route }: Props) {
   return (
-    <View style={styles.container}>
-      <Text>Overview </Text>
-      <Button title="Gå tillbaka" onPress={() => navigation.goBack()} />
-    </View>
+    <SafeAreaView style={[styles.container]}>
+      <Image source={require('../assets/geoappsplash.png')}></Image>
+      <View style={{ margin: 10 }}>
+        <Text style={{ fontSize: 29, fontWeight: 'bold' }}>
+          Quiz: Sample quiz #1
+        </Text>
+      </View>
+      <View style={{ margin: 10 }}>
+        <Text>Du har besvarat 7/10 frågor.</Text>
+      </View>
+      <View style={{ margin: 10 }}>
+        <Text>Antal steg: 4481</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          color={'#0c3663'}
+          title="Lämna in"
+          onPress={() => navigation.goBack()}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#adadad',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  buttonContainer: {
+    flex: 1,
+    bottom: 0,
+    marginBottom: 10,
+    // backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
   },
 });
